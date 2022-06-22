39b5d538eb6bfd787cc1679e19f263861771436e
wilgon456 <54878785+wilgon456@users.noreply.github.com>
2022-06-22 23:51:51 +0900

    220622_FirstServer_start

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🧜‍♂️`);
+
+app.listen(4000, handleListening);
