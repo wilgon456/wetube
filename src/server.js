f1b0b569ca48c21ca6db44d92430e848b52600a3
wilgon456 <juncatz1945@gmail.com>
2022-06-25 02:22:10 +0900

    220625_server_req&res

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I still love you❤️");
+}
+const handleLogin = (req, res) => {
+  return res.send("Lgoin here");
+}
+app.get("/",handleHome);
+app.get("/login",handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🧜‍♂️`);
 
