b7117d324fd4676be41b6362f9df3c7f41b81957
wilgon456 <juncatz1945@gmail.com>
2022-06-26 00:26:10 +0900

    220626_middleware_updated

--- src/server.js
+++ src/server.js
@@ -4,15 +4,34 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you❤️");
-}
-const handleLogin = (req, res) => {
-  return res.send("Lgoin here");
-}
-app.get("/",handleHome);
-app.get("/login",handleLogin);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next();
+};
 
+const privateMiddleware = (req, res, next) => {
+ const url = req.url
+ if(url === "/protected"){
+  return res.send("<h1>Not allowed</h1>");
+ }
+ console.log("Allowed, you may continue.")
+ next();
+}
+
+const handleHome = (req, res) => {
+  return res.send("<h1>Still Love you!</h1>");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+}
+
+//홈페이지는 get "/" 한다음 gossip 미들웨어 로딩 그 후 controller 구동
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🧜‍♂️`);
 
