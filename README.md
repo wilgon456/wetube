ef9277c88974bc19615928f54488b2d8c1e92ecb
wilgon456 <juncatz1945@gmail.com>
2022-06-27 12:38:20 +0900

    220627_router_updated

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wetube
  Clone youtube layout and system
+
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a video
+/videos/comments/delete -> Delete A comment of a Video
--- src/server.js
+++ src/server.js
@@ -5,18 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("i will respond.");
-  return res.send("Hello");
-} 
-const login = (req, res) => {
-  return res.send("login");
-}
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+const handleHome = (req,res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req,res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req,res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🧜‍♂️`);
